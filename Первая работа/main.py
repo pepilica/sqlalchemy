94d338071bf08d6fd6ee84fb108087693b47f3c8
pepilica <57628520+pepilica@users.noreply.github.com>
2020-03-05 19:24:16 +0300

    Первая работа

---
+++ Первая работа/main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from data import db_session
+from data.jobs import Jobs
+import datetime
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    session = db_session.create_session()
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
+    session.add(job)
+    session.commit()
+    app.run()
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/mars.sqlite")
+    main()
